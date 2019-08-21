6b579c113503c48f110e8a257b19064fb12e071b
Nate Maninger <me@n8m.us>
2019-08-21 12:30:15 +0900

    Change when address is generated to fix no addresses

--- src/data/wallet.js
+++ src/data/wallet.js
@@ -12,6 +12,11 @@ export async function refreshHostWallet() {
 			loadWalletAddress(),
 			loadWalletFees()
 		]);
+
+		if (!Store.state.lastAddress) {
+			await apiClient.createWalletAddress();
+			await loadWalletAddress();
+		}
 	} catch (ex) {
 		log.error('refreshHostWallet', ex.message);
 	}
@@ -34,72 +39,79 @@ async function unlockHostWalllet(password) {
 }
 
 async function loadWalletFees() {
-	const resp = await apiClient.getWalletAddresses();
+	try {
+		const resp = await apiClient.getWalletAddresses();
 
-	if (resp.statusCode !== 200)
-		throw new Error(resp.body.message);
+		if (resp.statusCode !== 200)
+			throw new Error(resp.body.message);
 
-	if (!Array.isArray(resp.body.addresses))
-		throw new Error('addresses must be an array');
+		if (!Array.isArray(resp.body.addresses))
+			throw new Error('addresses must be an array');
 
-	Store.dispatch('hostWallet/setFees', {
-		maximum: new BigNumber(resp.body.maximum),
-		minimum: new BigNumber(resp.body.minimum)
-	});
+		Store.dispatch('hostWallet/setFees', {
+			maximum: new BigNumber(resp.body.maximum),
+			minimum: new BigNumber(resp.body.minimum)
+		});
+	} catch (ex) {
+		log.error('loadWalletFees', ex.message);
+	}
 }
 
-async function loadWalletAddress(nocreate) {
-	const resp = await apiClient.getWalletAddresses();
+async function loadWalletAddress() {
+	try {
+		const resp = await apiClient.getWalletAddresses();
 
-	if (resp.statusCode !== 200)
-		throw new Error(resp.body.message);
+		if (resp.statusCode !== 200)
+			throw new Error(resp.body.message);
 
-	if (!Array.isArray(resp.body.addresses) || resp.body.addresses.length === 0) {
-		await apiClient.createWalletAddress();
-		return loadWalletAddress(true);
+		Store.dispatch('hostWallet/setLastAddress', resp.body.addresses[resp.body.addresses.length - 1]);
+	} catch (ex) {
+		log.error('loadWalletAddress', ex.message);
 	}
-
-	Store.dispatch('hostWallet/setLastAddress', resp.body.addresses[resp.body.addresses.length - 1]);
 }
 
 async function loadHostWallet(disableUnlock) {
-	const config = Store.state.config || {},
-		resp = await apiClient.getWallet(),
-		alerts = [];
+	try {
+		const config = Store.state.config || {},
+			resp = await apiClient.getWallet(),
+			alerts = [];
 
-	if (resp.statusCode !== 200)
-		throw new Error(resp.body.message);
+		if (resp.statusCode !== 200)
+			throw new Error(resp.body.message);
 
-	if (!resp.body.unlocked && resp.body.encrypted && !resp.body.rescanning && config.siad_wallet_password && !disableUnlock) {
-		unlockHostWalllet(config.siad_wallet_password);
-		await sleep(1);
-		await loadHostWallet(true);
-		return;
+		if (!resp.body.unlocked && resp.body.encrypted && !resp.body.rescanning && config.siad_wallet_password && !disableUnlock) {
+			unlockHostWalllet(config.siad_wallet_password);
+			await sleep(1);
+			await loadHostWallet(true);
+			return;
+		}
+
+		if (!resp.body.unlocked) {
+			alerts.push({
+				category: 'wallet',
+				severity: 'danger',
+				icon: 'wallet',
+				message: 'Wallet is not unlocked. Wallet must be unlocked to form new contracts'
+			});
+		}
+
+		if (resp.body.rescanning) {
+			alerts.push({
+				category: 'wallet',
+				severity: 'warning',
+				icon: 'wallet',
+				message: 'Wallet is rescanning. Host will not be able to form contracts until completed'
+			});
+		}
+
+		Store.dispatch('hostWallet/setAlerts', alerts);
+		Store.dispatch('hostWallet/setUnlocked', resp.body.unlocked);
+		Store.dispatch('hostWallet/setEncrypted', resp.body.encrypted);
+		Store.dispatch('hostWallet/setRescanning', resp.body.rescanning);
+		Store.dispatch('hostWallet/setHeight', resp.body.height);
+		Store.dispatch('hostWallet/setBalance', new BigNumber(resp.body.confirmedsiacoinbalance));
+		Store.dispatch('hostWallet/setBalanceDelta', new BigNumber(resp.body.unconfirmedincomingsiacoins).minus(resp.body.unconfirmedoutgoingsiacoins));
+	} catch (ex) {
+		log.error('loadHostWallet', ex.message);
 	}
-
-	if (!resp.body.unlocked) {
-		alerts.push({
-			category: 'wallet',
-			severity: 'danger',
-			icon: 'wallet',
-			message: 'Wallet is not unlocked. Wallet must be unlocked to form new contracts'
-		});
-	}
-
-	if (resp.body.rescanning) {
-		alerts.push({
-			category: 'wallet',
-			severity: 'warning',
-			icon: 'wallet',
-			message: 'Wallet is rescanning. Host will not be able to form contracts until completed'
-		});
-	}
-
-	Store.dispatch('hostWallet/setAlerts', alerts);
-	Store.dispatch('hostWallet/setUnlocked', resp.body.unlocked);
-	Store.dispatch('hostWallet/setEncrypted', resp.body.encrypted);
-	Store.dispatch('hostWallet/setRescanning', resp.body.rescanning);
-	Store.dispatch('hostWallet/setHeight', resp.body.height);
-	Store.dispatch('hostWallet/setBalance', new BigNumber(resp.body.confirmedsiacoinbalance));
-	Store.dispatch('hostWallet/setBalanceDelta', new BigNumber(resp.body.unconfirmedincomingsiacoins).minus(resp.body.unconfirmedoutgoingsiacoins));
 }
