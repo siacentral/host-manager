ee411019f8d016da7d577a3fe093afa27e4b70fc
Nate Maninger <me@n8m.us>
2019-11-12 09:57:37 +0900

    set automatic width to prevent wrapping issues

--- src/components/contracts/FilterPanel.vue
+++ src/components/contracts/FilterPanel.vue
@@ -302,7 +302,7 @@ export default {
 	top: 0;
 	right: 0;
 	bottom: 0;
-	width: 400px;
+	width: auto;
 	padding: 45px 15px 15px;
 	background-color: bg-dark-accent;
 	box-shadow: -2px 0px 5px rgba(0, 0, 0, 0.22);
