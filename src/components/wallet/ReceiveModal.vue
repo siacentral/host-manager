4f8c1b1480648ed3c12287ba9398d22005ee9b30
Nate Maninger <me@n8m.us>
2020-06-19 10:23:31 +0900

    add transak integration

--- src/components/wallet/ReceiveModal.vue
+++ src/components/wallet/ReceiveModal.vue
@@ -8,6 +8,9 @@
 				<input type="text" class="receive-address" :value="address" readonly />
 			</div>
 			<button class="btn btn-inline" @click="onCopyAddress"><icon icon="copy" /></button>
+			<div class="buy-button">
+				<button class="btn btn-inline" @click="onBuySiacoin">Buy Siacoin (PREVIEW)</button>
+			</div>
 		</div>
 	</modal>
 </template>
@@ -16,6 +19,7 @@
 import log from 'electron-log';
 import { mapState, mapActions } from 'vuex';
 import { clipboard } from 'electron';
+import Transak from '@transak/transak-sdk';
 
 import AddressQRCode from '@/components/wallet/AddressQRCode';
 import Modal from '@/components/Modal';
@@ -43,6 +47,39 @@ export default {
 			} catch (ex) {
 				log.error('receive siacoin copy', ex.message);
 			}
+		},
+		async onBuySiacoin() {
+			try {
+				const transak = new Transak({
+					apiKey: process.env.VUE_APP_TRANSAK_KEY,
+					environment: 'PRODUCTION',
+					cryptoCurrencyCode: 'SC',
+					walletAddress: this.address,
+					themeColor: '19cf86',
+					countryCode: 'US',
+					widgetHeight: `${window.innerHeight * 0.8}px`,
+					hostURL: window.location.origin
+				});
+
+				transak.init();
+
+				// To get all the events
+				transak.on(transak.ALL_EVENTS, (data) => {
+					console.log(data);
+				});
+
+				// This will trigger when the user marks payment is made.
+				transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
+					console.log(orderData);
+					transak.close();
+				});
+			} catch (ex) {
+				console.error('ReceiveModal.onBuySiacoin', ex);
+				this.pushNotification({
+					severity: 'danger',
+					message: ex.message
+				});
+			}
 		}
 	}
 };
@@ -56,7 +93,7 @@ export default {
 	grid-gap: 30px 15px;
 }
 
-.receive-qr-code {
+.receive-qr-code, .buy-button {
 	text-align: center;
 	grid-column: 1 / span 2;
 }
