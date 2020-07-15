eb88709c3d0b6d3cfaa6540d9ec1cf8cb51feebc
Nate Maninger <me@n8m.us>
2020-07-15 10:25:49 +0900

    disable async to generators

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
 module.exports = {
 	presets: [
-		'@vue/cli-plugin-babel/preset'
+		['@vue/cli-plugin-babel/preset',
+			{
+				exclude: ['@babel/plugin-transform-async-to-generator', '@babel/plugin-transform-regenerator']
+			}
+		]
+	],
+	plugins: [
+		['@babel/plugin-transform-runtime', {
+			regenerator: false
+		}],
+		['@babel/plugin-transform-regenerator', {
+			asyncGenerators: false,
+			generators: false,
+			async: false
+		}]
 	]
 };
