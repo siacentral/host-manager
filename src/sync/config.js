32cd0127f69be2d762c5d96af7bc36e12f5d343f
Nate Maninger <me@n8m.us>
2020-11-10 06:58:52 +0900

    overwrite maxcollateral if 0

--- src/sync/config.js
+++ src/sync/config.js
@@ -7,7 +7,35 @@ import Store from '@/store';
 export async function refreshHostConfig() {
 	try {
 		const host = await apiClient.getHost(),
-			alerts = [];
+			alerts = [],
+			config = {
+				acceptingcontracts: host.internalsettings.acceptingcontracts,
+				netaddress: host.internalsettings.netaddress,
+				maxduration: host.internalsettings.maxduration,
+				windowsize: host.internalsettings.windowsize,
+				maxrevisebatchsize: new BigNumber(host.internalsettings.maxrevisebatchsize),
+				maxdownloadbatchsize: new BigNumber(host.internalsettings.maxdownloadbatchsize),
+				mincontractprice: new BigNumber(host.internalsettings.mincontractprice),
+				mindownloadbandwidthprice: new BigNumber(host.internalsettings.mindownloadbandwidthprice),
+				minuploadbandwidthprice: new BigNumber(host.internalsettings.minuploadbandwidthprice),
+				minstorageprice: new BigNumber(host.internalsettings.minstorageprice),
+				collateral: new BigNumber(host.internalsettings.collateral),
+				maxcollateral: new BigNumber(host.internalsettings.maxcollateral),
+				collateralbudget: new BigNumber(host.internalsettings.collateralbudget),
+				minbaserpcprice: new BigNumber(host.internalsettings.minbaserpcprice),
+				minsectoraccessprice: new BigNumber(host.internalsettings.minsectoraccessprice),
+				registrysize: new BigNumber(host.internalsettings.registrysize),
+				customregistrypath: host.internalsettings.customregistrypath
+			};
+
+		if (config.maxcollateral.lte(0)) {
+			console.log('fixing max collateral');
+			apiClient.updateHost({
+				maxcollateral: config.collateral.times(4320).times(1e12).times(4).toFixed(0).toString(10)
+			});
+			await refreshHostConfig();
+			return;
+		}
 
 		if (!host.internalsettings.acceptingcontracts) {
 			alerts.push({
@@ -32,25 +60,7 @@ export async function refreshHostConfig() {
 
 		Store.dispatch('setNetAddress', host.externalsettings.netaddress);
 		Store.dispatch('hostConfig/setAlerts', alerts);
-		Store.dispatch('hostConfig/setConfig', {
-			acceptingcontracts: host.internalsettings.acceptingcontracts,
-			netaddress: host.internalsettings.netaddress,
-			maxduration: host.internalsettings.maxduration,
-			windowsize: host.internalsettings.windowsize,
-			maxrevisebatchsize: new BigNumber(host.internalsettings.maxrevisebatchsize),
-			maxdownloadbatchsize: new BigNumber(host.internalsettings.maxdownloadbatchsize),
-			mincontractprice: new BigNumber(host.internalsettings.mincontractprice),
-			mindownloadbandwidthprice: new BigNumber(host.internalsettings.mindownloadbandwidthprice),
-			minuploadbandwidthprice: new BigNumber(host.internalsettings.minuploadbandwidthprice),
-			minstorageprice: new BigNumber(host.internalsettings.minstorageprice),
-			collateral: new BigNumber(host.internalsettings.collateral),
-			maxcollateral: new BigNumber(host.internalsettings.maxcollateral),
-			collateralbudget: new BigNumber(host.internalsettings.collateralbudget),
-			minbaserpcprice: new BigNumber(host.internalsettings.minbaserpcprice),
-			minsectoraccessprice: new BigNumber(host.internalsettings.minsectoraccessprice),
-			registrysize: new BigNumber(host.internalsettings.registrysize),
-			customregistrypath: host.internalsettings.customregistrypath
-		});
+		Store.dispatch('hostConfig/setConfig', config);
 		Store.dispatch('hostConfig/setPriceTable', host.pricetable);
 	} catch (ex) {
 		log.error('refreshHostConfig', ex.message);
