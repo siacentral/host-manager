a75678d69b06c9782e79abdeb3a1e50947320cea
Nate <me@n8m.us>
2021-02-20 04:34:02 +0900

    sia v1.5.5 update

--- src/views/setup/SendSCStep.vue
+++ src/views/setup/SendSCStep.vue
@@ -14,9 +14,6 @@
 				<input type="text" class="receive-address" :value="address" readonly />
 			</div>
 			<button class="btn btn-inline" @click="onCopyAddress"><icon icon="copy" /></button>
-			<div class="buy-button">
-				<button class="btn btn-inline" @click="onBuySiacoin">Buy Siacoin</button>
-			</div>
 		</div>
 		<template v-slot:controls>
 			<button class="btn btn-success btn-inline" @click="$emit('done', { inc: 1 })">Done</button>
@@ -29,7 +26,6 @@
 import log from 'electron-log';
 import { mapState, mapActions } from 'vuex';
 import { clipboard } from 'electron';
-import Transak from '@transak/transak-sdk';
 import { formatSiacoinString, formatNumber } from '@/utils/formatLegacy';
 import BigNumber from 'bignumber.js';
 
@@ -90,45 +86,6 @@ export default {
 				});
 			} catch (ex) {
 				log.error('receive siacoin copy', ex.message);
-			}
-		},
-		async onBuySiacoin() {
-			try {
-				const transak = new Transak({
-					apiKey: process.env.VUE_APP_TRANSAK_KEY,
-					environment: 'PRODUCTION',
-					cryptoCurrencyCode: 'SC',
-					walletAddress: this.address,
-					themeColor: '19cf86',
-					countryCode: 'US',
-					widgetHeight: `${window.innerHeight * 0.8}px`,
-					hostURL: window.location.origin
-				});
-
-				transak.init();
-
-				// To get all the events
-				transak.on(transak.ALL_EVENTS, (data) => {
-					console.log(data);
-				});
-
-				// This will trigger when the user marks payment is made.
-				transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
-					console.log(orderData);
-					transak.close();
-				});
-
-				// This will trigger when the user closes the widget
-				transak.on(transak.EVENTS.TRANSAK_WIDGET_CLOSE, () => {
-					// transak widget annoyingly sets overflow to 'scroll' reset it back to auto
-					document.querySelector('html').style.overflow = 'auto';
-				});
-			} catch (ex) {
-				console.error('ReceiveModal.onBuySiacoin', ex);
-				this.pushNotification({
-					severity: 'danger',
-					message: ex.message
-				});
 			}
 		}
 	}
