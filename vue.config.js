91add38b7f236863a63f83123bb649cd9302f632
Nate Maninger <me@n8m.us>
2023-04-06 12:25:08 -0600

    ci: rename zen artifacts

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,144 @@
 const path = require('path');
 
+function electronOptionsMainnet() {
+	return {
+		outputDir: 'dist',
+		nodeIntegration: true,
+		builderOptions: {
+			appId: 'com.siacentral.host-desktop',
+			productName: 'Sia Host Manager',
+			copyright: '2019 Sia Central',
+			afterSign: 'build/scripts/notarize.js',
+			afterAllArtifactBuild: 'build/scripts/sign.js',
+			/* eslint-disable no-template-curly-in-string */
+			artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
+			extraResources: [
+				{
+					/* eslint-disable no-template-curly-in-string */
+					from: 'build/bin/${os}/${arch}',
+					to: 'bin',
+					filter: [
+						'**/*'
+					]
+				}
+			],
+			generateUpdatesFilesForAllChannels: true,
+			win: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-v${version}-${os}-${arch}.${ext}'
+			},
+			mac: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
+				hardenedRuntime: true,
+				// disabled due to new Apple notarization failing
+				gatekeeperAssess: false,
+				entitlements: 'build/entitlements.mac.plist',
+				entitlementsInherit: 'build/entitlements.mac.plist'
+			},
+			linux: {
+				executableName: 'Sia Host Manager',
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
+				target: [
+					'deb',
+					'AppImage'
+				],
+				category: 'Utility'
+			},
+			appImage: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-v${version}-${os}-${arch}.${ext}'
+			},
+			dmg: {
+				// new apple notarization does not need the dmg signed
+				sign: false
+			},
+			nsis: {
+				oneClick: true,
+				perMachine: true
+			},
+			publish: {
+				provider: 's3',
+				bucket: 'siacentral',
+				endpoint: 'https://s3.filebase.com',
+				path: '/releases/host-manager'
+			}
+		}
+	};
+}
+
+function electronOptionsZen() {
+	return {
+		outputDir: 'dist',
+		nodeIntegration: true,
+		builderOptions: {
+			appId: 'com.siacentral.host-desktop',
+			productName: 'Sia Host Manager - Zen',
+			copyright: '2023 Sia Central',
+			afterSign: 'build/scripts/notarize.js',
+			afterAllArtifactBuild: 'build/scripts/sign.js',
+			/* eslint-disable no-template-curly-in-string */
+			artifactName: '${productName}-zen-v${version}-${os}-${arch}.${ext}',
+			extraResources: [
+				{
+					/* eslint-disable no-template-curly-in-string */
+					from: 'build/bin/${os}/${arch}',
+					to: 'bin',
+					filter: [
+						'**/*'
+					]
+				}
+			],
+			generateUpdatesFilesForAllChannels: true,
+			win: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-zen-v${version}-${os}-${arch}.${ext}'
+			},
+			mac: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-zen-v${version}-${os}-${arch}.${ext}',
+				hardenedRuntime: true,
+				// disabled due to new Apple notarization failing
+				gatekeeperAssess: false,
+				entitlements: 'build/entitlements.mac.plist',
+				entitlementsInherit: 'build/entitlements.mac.plist'
+			},
+			linux: {
+				executableName: 'Sia Host Manager - Zen',
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-zen-v${version}-${os}-${arch}.${ext}',
+				target: [
+					'deb',
+					'AppImage'
+				],
+				category: 'Utility'
+			},
+			appImage: {
+				/* eslint-disable no-template-curly-in-string */
+				artifactName: '${productName}-zen-v${version}-${os}-${arch}.${ext}'
+			},
+			dmg: {
+				// new apple notarization does not need the dmg signed
+				sign: false
+			},
+			nsis: {
+				oneClick: true,
+				perMachine: true
+			}
+		}
+	};
+}
+
+function networkElectronOptions() {
+	switch (process.env.HOST_MANAGER_BUILD_NETWORK) {
+	case 'zen':
+		return electronOptionsZen();
+	default:
+		return electronOptionsMainnet();
+	}
+}
+
 module.exports = {
 	parallel: false,
 	chainWebpack: config => {
@@ -9,71 +148,7 @@ module.exports = {
 		types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)));
 	},
 	pluginOptions: {
-		electronBuilder: {
-			outputDir: 'dist',
-			nodeIntegration: true,
-			builderOptions: {
-				appId: 'com.siacentral.host-desktop',
-				productName: 'Sia Host Manager',
-				copyright: '2019 Sia Central',
-				afterSign: 'build/scripts/notarize.js',
-				afterAllArtifactBuild: 'build/scripts/sign.js',
-				/* eslint-disable no-template-curly-in-string */
-				artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
-				extraResources: [
-					{
-						/* eslint-disable no-template-curly-in-string */
-						from: 'build/bin/${os}/${arch}',
-						to: 'bin',
-						filter: [
-							'**/*'
-						]
-					}
-				],
-				generateUpdatesFilesForAllChannels: true,
-				win: {
-					/* eslint-disable no-template-curly-in-string */
-					artifactName: '${productName}-v${version}-${os}-${arch}.${ext}'
-				},
-				mac: {
-					/* eslint-disable no-template-curly-in-string */
-					artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
-					hardenedRuntime: true,
-					// disabled due to new Apple notarization failing
-					gatekeeperAssess: false,
-					entitlements: 'build/entitlements.mac.plist',
-					entitlementsInherit: 'build/entitlements.mac.plist'
-				},
-				linux: {
-					executableName: 'Sia Host Manager',
-					/* eslint-disable no-template-curly-in-string */
-					artifactName: '${productName}-v${version}-${os}-${arch}.${ext}',
-					target: [
-						'deb',
-						'AppImage'
-					],
-					category: 'Utility'
-				},
-				appImage: {
-					/* eslint-disable no-template-curly-in-string */
-					artifactName: '${productName}-v${version}-${os}-${arch}.${ext}'
-				},
-				dmg: {
-					// new apple notarization does not need the dmg signed
-					sign: false
-				},
-				nsis: {
-					oneClick: true,
-					perMachine: true
-				},
-				publish: {
-					provider: 's3',
-					bucket: 'siacentral',
-					endpoint: 'https://s3.filebase.com',
-					path: '/releases/host-manager'
-				}
-			}
-		}
+		electronBuilder: networkElectronOptions()
 	}
 };
 
